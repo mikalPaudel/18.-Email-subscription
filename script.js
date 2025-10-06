6279d490e54890e6cf2355b05dc4a08b95036d74
Mikal Paudel <mikalpaudel58@gmail.com>
2025-10-06 14:15:04 +0545

    Show/hide msg. Error in tackling invalid email input

--- script.js
+++ script.js
@@ -7,3 +7,21 @@
       .then(response => console.log('Success!', response))
       .catch(error => console.error('Error!', error.message))
   })
+
+  const emails = document.getElementById('email');
+  const submit = document.getElementById('submit');
+  const thankMsg = document.querySelector('span');
+
+  submit.addEventListener('click', ()=>{
+    if(!emails == `/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/`){
+        thankMsg.innerHTML = 'Please Enter Valid Email';
+        thankMsg.style.color = 'red';
+        return false;
+    }
+    else{
+        thankMsg.innerHTML = 'Thank You For Subscribing';
+        emails.value = '';
+        return true;
+        
+    }
+  })
